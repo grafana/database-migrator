2f00cdea0e3498a7e409fd1de44eadac562667bf
eleijonmarck <eric.leijonmarck@gmail.com>
2022-10-06 23:24:21 +0100

    add: parsing of double qoutes to backtick for reserved names

--- sqlitedump.sh
+++ sqlitedump.sh
@@ -7,4 +7,4 @@ done
 for t in $TABLES; do
     echo -e ".headers on"
     echo -e ".mode insert $t\nselect * from $t;"  
-done | sqlite3 $DB | sed -e 's/\\[rnut"]/\\&/g'
+done | sqlite3 $DB | sed -e 's/\\[rnut"]/\\&/g' |  awk -F VALUES '{ gsub(/"/, "`", $1); print $1 "VALUES" $2 }'
