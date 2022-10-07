0c282afb3606115a9ea3198ccb601ba4331d3601
Dan Cech <dcech@grafana.com>
2022-10-07 13:51:45 -0400

    update inline code documentation

--- sqlitedump.sh
+++ sqlitedump.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 DB=$1
-# FIXME: explain the seds purpose here
+# get list of tables as a single column without migration_log
 TABLES=$(sqlite3 $DB .tables | sed -r 's/(\S+)\s+(\S)/\1\n\2/g' | grep -v migration_log)
+# output sql to truncate all target tables
 for t in $TABLES; do
     echo "TRUNCATE TABLE $t;"
 done
+# output sql to insert data into all target tables
 for t in $TABLES; do
     echo -e ".headers on"
     echo -e ".mode insert $t\nselect * from $t;"  
-# sed is for removing the backslag \t,r,n,t" from the file
-# awk is for substituting " for backtick for the column names of the INSERT statements
+# sed is for adding a backslash to \t,\r,\n,\t & \" so they are properly escaped for MySQL
+# awk is to convert double-quotes to backticks to properly quote reserved column names for MySQL
 done | sqlite3 $DB | sed -e 's/\\[rnut"]/\\&/g' |  awk -F VALUES '{ gsub(/"/, "`", $1); print $1 "VALUES" $2 }'
